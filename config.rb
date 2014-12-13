9f35734bb1d585cc8f6f5bde8620e174db9137a7
James Seymour-Lock <james@simpleasmilk.co.uk>
2014-12-13 12:48:57 +0100

    basic setup of locations

--- config.rb
+++ config.rb
@@ -53,6 +53,8 @@ set :js_dir, 'javascripts'
 
 set :images_dir, 'images'
 
+
+
 # Build-specific configuration
 configure :build do
   # For example, change the Compass output style for deployment
@@ -69,4 +71,17 @@ configure :build do
 
   # Or use a different image path
   # set :http_prefix, "/Content/images/"
+
+  data.events.each do |id, meetup|
+    if meetup.published == false
+      ignore "/#{id.dasherize}.html"
+    end
+  end
 end
+
+
+
+
+
+
+
